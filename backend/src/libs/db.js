0da3f02c94dcdb7d11d9c3f91d042e1cbdf569cb
Tech Runner By SJ <137867584+techRunnerBySJ@users.noreply.github.com>
2025-05-04 00:24:36 +0530

    first commit

---
+++ backend/src/libs/db.js
@@ -0,0 +1,18 @@
+import {PrismaClient} from '../generated/prisma/index.js';
+import {Prisma} from '@prisma/client';
+
+const globalForPrisma = globalThis;
+
+export const db = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = db;
+}
+
+export const dbErrorHandler = (error) => {
+  if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    if (error.code === 'P2002') {
+      throw new Error('Unique constraint failed');
+    }
+  }
+};
